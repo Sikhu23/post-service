320e379f7d6c2b84a028c01a0fce8cbd7769f066
Siddharth Singh <siddharths@maveric-systems.com>
2022-03-21 21:12:12 +0530

    feature added - get post by id

--- src/main/java/com/postservice/Controller/PostController.java
+++ src/main/java/com/postservice/Controller/PostController.java
@@ -1,8 +1,13 @@
 package com.postservice.Controller;
 
 
+import com.postservice.Model.PostModel;
 import com.postservice.Service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +15,9 @@ public class PostController {
 
     @Autowired
     private PostService postService;
+
+    @GetMapping("/posts/{postId}")
+    public ResponseEntity<PostModel> findById(@PathVariable("postId") String postId){
+        return new ResponseEntity<>(postService.findById(postId), HttpStatus.ACCEPTED);
+    }
 }
--- src/main/java/com/postservice/Service/PostService.java
+++ src/main/java/com/postservice/Service/PostService.java
@@ -1,6 +1,7 @@
 package com.postservice.Service;
 
 
+import com.postservice.Model.PostModel;
 import com.postservice.Repository.PostRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,4 +11,8 @@ public class PostService {
 
     @Autowired
     private PostRepo postRepo;
+
+    public PostModel findById(String postId){
+        return this.postRepo.findById(postId).get();
+    }
 }
