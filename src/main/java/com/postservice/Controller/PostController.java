de5c156bea5c9ce5bd780e2229bd551464ff3a4f
Siddharth Singh <siddharths@maveric-systems.com>
2022-03-21 20:52:42 +0530

    feature added - post a new post

--- src/main/java/com/postservice/Controller/PostController.java
+++ src/main/java/com/postservice/Controller/PostController.java
@@ -1,13 +1,31 @@
 package com.postservice.Controller;
 
 
+import com.postservice.Model.PostModel;
 import com.postservice.Service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 public class PostController {
 
     @Autowired
     private PostService postService;
+
+
+    @PostMapping("/posts")
+    public ResponseEntity<PostModel> savePost(@RequestBody @Valid PostModel postModel){
+        return  new ResponseEntity<>(postService.savePost(postModel), HttpStatus.ACCEPTED);
+    }
+
+
+
+
+
 }
