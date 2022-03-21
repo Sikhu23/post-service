f247b597cde119e6f449b9edbf2073c8249fb404
Siddharth Singh <siddharths@maveric-systems.com>
2022-03-21 21:28:16 +0530

    feature added - put post by id i.e update the post

--- src/main/java/com/postservice/Controller/PostController.java
+++ src/main/java/com/postservice/Controller/PostController.java
@@ -1,13 +1,27 @@
 package com.postservice.Controller;
 
 
+import com.postservice.Model.PostModel;
 import com.postservice.Service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 public class PostController {
 
     @Autowired
     private PostService postService;
+
+
+    @PutMapping("/posts/{postId}")
+    public ResponseEntity<PostModel> updatePost(@RequestBody @Valid PostModel postModel, @PathVariable("postId") String postId){
+        return  new ResponseEntity<>(postService.updatePost(postModel,postId), HttpStatus.ACCEPTED);
+    }
 }
--- src/main/java/com/postservice/Service/PostService.java
+++ src/main/java/com/postservice/Service/PostService.java
@@ -1,13 +1,23 @@
 package com.postservice.Service;
 
 
+import com.postservice.Model.PostModel;
 import com.postservice.Repository.PostRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class PostService {
 
     @Autowired
     private PostRepo postRepo;
+
+    public PostModel updatePost(PostModel postModel, String postId){
+
+        postModel.setCreatedAt(postRepo.findById(postId).get().getCreatedAt());
+        postModel.setUpdatedAt(LocalDateTime.now());
+        return this.postRepo.save(postModel);
+    }
 }
