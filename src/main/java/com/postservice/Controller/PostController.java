2a3a5e33b60d473eeec5211fd498737f78cd0258
Siddharth Singh <siddharths@maveric-systems.com>
2022-03-22 11:02:51 +0530

    removed some dependency

--- src/main/java/com/postservice/Controller/PostController.java
+++ src/main/java/com/postservice/Controller/PostController.java
@@ -1,13 +1,21 @@
 package com.postservice.Controller;
 
 
+import com.postservice.Model.PostModel;
 import com.postservice.Service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class PostController {
 
     @Autowired
     private PostService postService;
+
+   
 }
--- src/main/java/com/postservice/Service/PostService.java
+++ src/main/java/com/postservice/Service/PostService.java
@@ -1,13 +1,18 @@
 package com.postservice.Service;
 
 
+import com.postservice.Model.PostModel;
 import com.postservice.Repository.PostRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PostService {
 
     @Autowired
     private PostRepo postRepo;
+
+
 }
